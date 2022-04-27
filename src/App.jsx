c69e37983cc3b1f5e48c8291a9b43b67add98fc6
omer <peynirciomerfaruk@gmail.com>
2022-04-27 16:54:57 +0200

    employees are on screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-
-
-import './App.scss'
+import "./App.scss";
+import employees from "./data/employees.json";
 
 function App() {
-  
-
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <h1>Employees</h1>
+      {employees.map((employee)=>{
+       return <li>{employee.firstName+" "+employee.lastName}</li>;
+      })}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
